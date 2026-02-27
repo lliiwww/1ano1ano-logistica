0513e3183b2c0b09bf770d25e967c9dc33d2f734
lliiwww <isolatolucas@gmail.com>
2026-02-27 03:17:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
 // popup.js
 
-// Pega os elementos do HTML
 const openBtn = document.getElementById('openCredits');
 const closeBtn = document.getElementById('closeCredits');
 const popup = document.getElementById('creditsPopup');
 const overlay = document.getElementById('overlay');
 
-// Abre o pop-up quando clica no botão
 openBtn.addEventListener('click', () => {
   popup.style.display = 'block';
   overlay.style.display = 'block';
 });
 
-// Fecha o pop-up ao clicar no botão "Fechar"
 closeBtn.addEventListener('click', () => {
   popup.style.display = 'none';
   overlay.style.display = 'none';
 });
 
-// Fecha o pop-up ao clicar no fundo escuro
 overlay.addEventListener('click', () => {
   popup.style.display = 'none';
   overlay.style.display = 'none';
